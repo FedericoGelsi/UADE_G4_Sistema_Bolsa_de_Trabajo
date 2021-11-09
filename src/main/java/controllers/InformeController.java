73fe07fc2fa40b7e6ad1083bcb1e0574b099b2c5
chuaman <cecilia.amparohm@gmail.com>
2021-11-09 18:34:21 -0300

    first commit

--- src/main/java/controllers/InformeController.java
+++ src/main/java/controllers/InformeController.java
@@ -1,4 +1,26 @@
 package controllers;
 
+import models.vo.InformeVO;
+import models.vo.OfertaLaboralVO;
+
+import java.util.List;
+
 public class InformeController {
+    private List<OfertaLaboralVO> ofertasLaboralesVO;
+
+    public InformeController() {
+    }
+
+    public List<OfertaLaboralVO> getOfertasLaboralesVO() {
+        return ofertasLaboralesVO;
+    }
+
+    public void setOfertasLaboralesVO(List<OfertaLaboralVO> ofertasLaboralesVO) {
+        this.ofertasLaboralesVO = ofertasLaboralesVO;
+    }
+
+    public InformeVO getInformeOfertaMasAccesible(){
+
+        return null;
+    }
 }
